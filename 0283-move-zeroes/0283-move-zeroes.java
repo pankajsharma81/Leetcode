2aae4bc19c7135063775193c73b7ac02848b7d99
Pankaj Kumar Sharma <137093233+pankajsharma81@users.noreply.github.com>
2024-10-15 19:10:07 +0530

    Time: 2 ms (85.69%), Space: 45.9 MB (33.8%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,17 +1,14 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        //index of first place that is not taken yet for non zero elements
-        int j = 0;
-        int n = nums.length;
-        for(int i =0;i<n;i++){
-            //if not a zero then place it in most soon location
-            if(nums[i]!=0){
-                nums[j] = nums[i];
-                j++;
+        int nonZeroIdx = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[nonZeroIdx] = nums[i];
+                nonZeroIdx++;
             }
         }
-        //filling rest as zeros
-        for(int i = j;i<n;i++){
+
+        for (int i = nonZeroIdx; i < nums.length; i++) {
             nums[i] = 0;
         }
     }
